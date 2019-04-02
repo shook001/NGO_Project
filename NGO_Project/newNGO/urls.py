c830c3cf6501992b22cea3f176183081a4ff71c5
xiaochen hu <jason95328@hotmail.com>
2019-04-02 11:51:51 -0400

    Image updates
    Apil 2nd 2019 11:51 am

--- NGO_Project/newNGO/urls.py
+++ NGO_Project/newNGO/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('events/', views.events, name='events'),
     path('event-detail/<int:id>/', views.event_detail, name='event_detail'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-]
