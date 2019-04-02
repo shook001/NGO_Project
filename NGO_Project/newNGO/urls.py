246307450784ee6ae6bc72ca37d336fa0c1da9c2
xiaochen hu <jason95328@hotmail.com>
2019-04-02 15:14:42 -0400

    urls fixed

--- NGO_Project/newNGO/urls.py
+++ NGO_Project/newNGO/urls.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic.base import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
 from app import views
-
 
 urlpatterns = [
     path('', views.events, name='events'),
@@ -13,7 +11,5 @@ urlpatterns = [
     path('confirmation/', views.confirm, name='confirmation'),
     path('event-detail/<int:id>/', views.event_detail, name='event_detail'),
     path('register/', views.post),
-    path('event-details/', TemplateView.as_view(template_name='event_details.html'), name='event-details'),
-    path('price/', TemplateView.as_view(template_name='total_price.html'), name='price'),
-    path('conformation/', TemplateView.as_view(template_name='confirm.html'), name='conformation'),
+    path('price/', views.price, name='price')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
