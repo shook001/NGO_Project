6ae944774cac537f28bab3f5e03b281d121d82bd
dabubbla <ejohnston86@gmail.com>
2019-04-02 11:52:24 -0400

    Added views for login and confirmation, changed events to our homepage. Login link in upper right directs user to login page

--- NGO_Project/app/views.py
+++ NGO_Project/app/views.py
@@ -1,17 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
 
-from .models import Event
+from .models import Event, User
 
-
-def events(request):
+# Events will act as homepage
+def events(request): # good
 	events = Event.objects.all()
 	return render(request, 'events.html', {'events': events})
 
-
-def event_detail(request, id):
+# Selected object will be directed here
+def event_detail(request, id):# good
 	try:
 		event = Event.objects.get(id=id)
 	except Event.DoesNotExist:
 		raise Http404('Event not found!!')
 	return render(request, 'event-detail.html', {'event': event})
+
+
+# Accounts/login
+def login(request):
+	return render(request, 'registration.login.html')
+
+# After user selects ticket amounts we need to provide a sum/total
+def total(request):
+	pass
+
+
+# confirmation that order was recognized
+def confirm(request):# good
+	return render(request, 'confirm.html')
--- NGO_Project/newNGO/urls.py
+++ NGO_Project/newNGO/urls.py
@@ -1,17 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic.base import TemplateView
+# from django.views.generic.base import TemplateView
 
 from app import views
 
 urlpatterns = [
+    path('', views.events, name='events'),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('events/', views.events, name='events'),
+    path('confirmation/', views.confirm, name='confirmation'),
     path('event-detail/<int:id>/', views.event_detail, name='event_detail'),
-    path('user-view/', TemplateView.as_view(template_name='user_view.html'), name='event'),
-    path('event-details/', TemplateView.as_view(template_name='event_details.html'), name='event-details'),
-    path('register/', TemplateView.as_view(template_name='register.html'), name='register'),
-    path('price/', TemplateView.as_view(template_name='total_price.html'), name='price'),
-    path('conformation/', TemplateView.as_view(template_name='confirm.html'), name='conformation'),
+
+    #Must define these within views.py
+    # path('register/', views.register, name='register'),
+    # path('price/', views.total, name='total'),#must work on algo for this!!!!
 ]
